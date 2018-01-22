c5e70919481f51ea27f8d02e5563e5817a228107
Tim Chen <timchen1@gmail.com>
2018-01-22 15:18:17 -0500

    include dist files for custom fork

---
+++ dist/FieldArray.d.ts
@@ -0,0 +1,39 @@
+/// <reference types="react" />
+import * as React from 'react';
+import { SharedRenderProps } from './types';
+import * as PropTypes from 'prop-types';
+import { FormikProps } from './formik';
+export declare type FieldArrayConfig = {
+  name: string;
+} & SharedRenderProps<
+  ArrayHelpers & {
+    form: FormikProps<any>;
+  }
+>;
+export interface ArrayHelpers {
+  push: (obj: any) => void;
+  swap: (indexA: number, indexB: number) => void;
+  move: (from: number, to: number) => void;
+  insert: (index: number, value: any) => void;
+  unshift: (value: any) => number;
+  remove<T>(index: number): T | undefined;
+  pop<T>(): T | undefined;
+}
+export interface FieldArrayState {}
+export declare class FieldArray extends React.Component<
+  FieldArrayConfig,
+  FieldArrayState
+> {
+  static contextTypes: {
+    formik: PropTypes.Requireable<any>;
+  };
+  changeValue: (fn: Function) => void;
+  push: (value: any) => void;
+  swap: (indexA: number, indexB: number) => void;
+  move: (from: number, to: number) => void;
+  insert: (index: number, value: any) => void;
+  unshift: (value: any) => number;
+  remove: (index: number) => undefined;
+  pop: () => undefined;
+  render(): any;
+}
---
+++ dist/Mutators.d.ts
@@ -0,0 +1,7 @@
+export declare const move: (array: any[], from: number, to: number) => any[];
+export declare const swap: (
+  array: any[],
+  indexA: number,
+  indexB: number
+) => any[];
+export declare const insert: (array: any[], index: number, value: any) => any[];
